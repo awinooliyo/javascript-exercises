debde7a832a4d0b7dd43fdbbfc967e4d5fd6fab7
Awino Oliyo <erick.otieno89@gmail.com>
2024-08-06 17:45:11 +0000

    removing from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // Create a new empty array
+    const newArray = [];
+    // Loop through the new array with for each
+    array.forEach((item) => {
+      // Push every element into the array
+      // Unless the element is in the args
+      // Create a new array with new items except those to be removed
+      if (!args.includes(item)) {
+          newArray.push(item);
+      }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
