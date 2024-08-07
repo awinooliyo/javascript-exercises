428b108ca253fd643cbbcea497d36133b2e9c40b
Awino Oliyo <erick.otieno89@gmail.com>
2024-08-07 06:43:49 +0000

    Finding leap year - boolean

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+// A Leap year is dividible by 4.
+// If it is divisible by 100 it must also be divisible by 400 for it to be a leap year.
+// If the year is divisible by 4 and not divisible by 100, it is a leap year.
+// If the year is divisible by 400, it is a leap year.
+// If the year is divisible by 100 but not by 400, it is not a leap year.
+// If the year is not divisible by 4, it is not a leap year.
+// Below is the implementation. 
+const leapYears = function(year) {
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
