bab0e652f8c4903cd55094d7fa3628bd771f2652
Awino Oliyo <erick.otieno89@gmail.com>
2024-08-07 07:29:28 +0000

    converting temperature C to F and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    const celsius = (fahrenheit - 32) * (5 / 9);
+    return Math.round(celsius * 10) / 10; // Rounding to 1 decimal place
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+    const fahrenheit = (celsius * (9 / 5)) + 32;
+    return Math.round(fahrenheit * 10) / 10; // Rounding to 1 decimal place
 };
 
 // Do not edit below this line
